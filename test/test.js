e81ecb82aed6895bece052e1e6b465821c312acf
CookieWorkProject <cookieworkproject@gmail.com>
2026-03-23 23:37:22 -0400

    fixed testing

--- test/test.js
+++ test/test.js
@@ -4,25 +4,25 @@ const assert = require('assert');
 GameState.initGame();
 
 //Movement Test
-assert.strictEqual(GameState.player.xPos, 8,"test");
-assert.strictEqual(GameState.player.yPos, 8,"test");
+assert.strictEqual(GameState.player.xPos, 8);
+assert.strictEqual(GameState.player.yPos, 8);
 GameState.gameStep("r");
-assert.strictEqual(GameState.player.xPos, 9,"test");
-assert.strictEqual(GameState.player.yPos, 8,"test");
+assert.strictEqual(GameState.player.xPos, 9);
+assert.strictEqual(GameState.player.yPos, 8);
 GameState.gameStep("l");
-assert.strictEqual(GameState.player.xPos, 8,"test");
-assert.strictEqual(GameState.player.yPos, 8,"test");
+assert.strictEqual(GameState.player.xPos, 8);
+assert.strictEqual(GameState.player.yPos, 8);
 GameState.gameStep("u");
-assert.strictEqual(GameState.player.xPos, 8,"test");
-assert.strictEqual(GameState.player.yPos, 7,"test");
+assert.strictEqual(GameState.player.xPos, 8);
+assert.strictEqual(GameState.player.yPos, 7);
 GameState.gameStep("d");
-assert.strictEqual(GameState.player.xPos, 8,"test");
-assert.strictEqual(GameState.player.yPos, 8,"test");
+assert.strictEqual(GameState.player.xPos, 8);
+assert.strictEqual(GameState.player.yPos, 8);
 
 //Quit Test
 GameState.initGame();
 GameState.gameStep("q");
-assert.strictEqual(GameState.gameover, true,"test");
+assert.strictEqual(GameState.gameover, true);
 
 
 //Win Test
@@ -31,4 +31,4 @@ for (let i = 0; i < 4; i++) {
 GameState.gameStep("r");
 GameState.gameStep("u");
 }
-assert.strictEqual(GameState.gameover, true,"test");
+assert.strictEqual(GameState.gameover, true);
